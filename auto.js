4f69f548e5343235ec99b8e05e44d4f2ac8c245e
Bernard <b.mullerjnr@gmail.com>
2020-12-24 10:03:32 +0200

    credentials

--- auto.js
+++ auto.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const ExcelJS = require('exceljs');
+const prompt = require('prompt-sync')();
 // import {months} from 'months';
 
 const months = {
@@ -20,28 +21,30 @@ const months = {
 const date = new Date();
 const day = date.toISOString().slice(8,10);
 const month = months[date.toISOString().slice(5,7)];
+// const month = 'July';
 const year = date.toISOString().slice(0,4);
 const progDir = process.cwd();
 // const tempPath = progDir + '/templates/template.xlsx';
-const bookName = year + '_timebook.xslx';
+const bookName = year + '_timebook.xlsx';
 const bookPath = progDir + '/timesheets/' + bookName;
 const weekNum = Math.floor((day - 1) / 7 + 1);
+const timebook = new ExcelJS.Workbook();
+
+
 
 
 class Setup {
-    constructor (year ,bookPath, bookName, tempPath) {
-        // this.year = year;
-        // this.bookPath = bookPath;
-        // this.bookName = bookName;
-        // this.tempPath = tempPath;
+    constructor () {
     }
 
-    checkDirs(bookPath) {
+    initialize() {
         if (!fs.existsSync(bookPath)) {
             console.log('Timebook does not exist');
             this.createBook();
-        }      
-    }
+            this.getActiveSheet();
+            // this.getCredentials();
+        }       
+    }    
 
     moveToTimesheets() {
         fs.rename(progDir + `/${bookName}`, bookPath, function(err){
@@ -50,13 +53,54 @@ class Setup {
         })
     }
 
-    async createBook() {
-        const timebook = new ExcelJS.Workbook();
-        const template = timebook.addWorksheet('TEMPLATE');
-        await timebook.xlsx.writeFile(bookName);     
+    async createBook() {       
+        this.saveFile();
         this.moveToTimesheets();
+    }
+
+    async getActiveSheet() {
+        if (!timebook.worksheets.includes(month)) {
+            let activeSheet = timebook.addWorksheet(`${month}`);
+            console.log(`Sheet: ${activeSheet.name} created`)
+        } else {
+            return timebook.getWorksheet(`${month}`);
+        }
+    }
+
+    async saveFile() {
+        await timebook.xlsx.writeFile(bookName); 
+    }
+
+    getCredentials() {
+        let userName = prompt('What is your name?');
+        console.log(`Hello, ${userName}!`)
     }
 }
 
-let timebook = new Setup();
-timebook.checkDirs()
+
+
+class Styler {
+    constructor() {
+        
+    }
+    
+    sizeColumns() {
+        let activeSheet = timebook.getWorksheet(`${month}`)
+        activeSheet.getColumn('A').width = 17;
+        activeSheet.getColumn('B').width = 48;
+        activeSheet.getColumn('C').width = 17;
+        console.log('column sizes changed')
+    }
+}
+
+
+let setup = new Setup();
+let styler = new Styler();
+setup.initialize()
+styler.sizeColumns()
+setup.saveFile();
+
+    
+
+
+
