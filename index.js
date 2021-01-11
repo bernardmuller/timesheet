8963395fcadf54b6678a16e7d9fd7a1f2489d8c3
grizzlyui <Muller@1996>
2021-01-11 15:26:15 +0200

    'server-setup'

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('timesheet/index');
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+})
